f5e9c8e45985e65835b58af3125b8810bec46b0a
Jeet Soni <jeet.soni0822@gmail.com>
2023-09-24 12:17:13 -0400

    fixed function calling error

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -5,7 +5,7 @@ def number_check(number):
     """Method to check if user entered negative number"""
 
     if number < 0:
-        raise (NegativeValueError(number))
+        raise (NegativeValueError)
 
 
 def main():
@@ -14,9 +14,10 @@ def main():
     # try and except block to take user input and check it
     try:
         num = int(input("Enter a number: "))
-        print(f"your positive number is: {number_check(num)}")  # print number
+        number_check(num)
+        print(f"your positive number is: {num}")  # print number
     except NegativeValueError:
-        print("sorry, number is negative")  # error rasied
+        print("sorry, the number is negative")  # error rasied
 
 
 if __name__ == "__main__":
